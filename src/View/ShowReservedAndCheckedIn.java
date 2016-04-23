a3d4922f354bf1eccf3e28fb6e75b82f893cfb2b
pornratp <dai_someone@hotmail.com>
2016-04-23 18:27:49 +0700

    Make ShowReservedAndCheckedIn in View Package

---
+++ src/View/ShowReservedAndCheckedIn.java
@@ -0,0 +1,244 @@
+package View;
+
+import Controller.Controller;
+import Model.RestForm;
+import java.util.ArrayList;
+import java.util.Date;
+import javax.swing.JTable;
+
+public class ShowReservedAndCheckedIn extends javax.swing.JPanel {
+
+    Controller controller;
+    ArrayList<RestForm> rests;
+
+    public ShowReservedAndCheckedIn(Controller controller, ArrayList<RestForm> rests) {
+        this.controller = controller;
+        this.rests = rests;
+        initComponents();
+        setBounds(0, 0, 800, 600);
+        add(new Header("Show Reserved and Checked In"));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        emptyPanel = new javax.swing.JPanel();
+        lblStatus = new javax.swing.JLabel();
+        cbbStatus = new javax.swing.JComboBox();
+        btnSearch = new javax.swing.JButton();
+        scrollPaneForTable = new javax.swing.JScrollPane();
+        table = new javax.swing.JTable();
+        btnDetail = new javax.swing.JButton();
+        btnCancel = new javax.swing.JButton();
+        lblDate = new javax.swing.JLabel();
+        cbtnDate = new javax.swing.JCheckBox();
+        calendarDate = new org.freixas.jcalendar.JCalendarCombo();
+
+        setBackground(new java.awt.Color(255, 204, 0));
+
+        emptyPanel.setBackground(new java.awt.Color(255, 255, 255));
+
+        lblStatus.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        lblStatus.setText("Status");
+
+        cbbStatus.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "-", "Checking-In", "Reserved" }));
+
+        btnSearch.setBackground(new java.awt.Color(255, 204, 0));
+        btnSearch.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        btnSearch.setText("Search");
+        btnSearch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSearchActionPerformed(evt);
+            }
+        });
+
+        table.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Social No", "First Name", "Last Name", "Start Date", "End Date", "Status"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        table.setAutoscrolls(false);
+        table.setColumnSelectionAllowed(true);
+        table.getTableHeader().setReorderingAllowed(false);
+        scrollPaneForTable.setViewportView(table);
+        table.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        if (table.getColumnModel().getColumnCount() > 0) {
+            table.getColumnModel().getColumn(0).setResizable(false);
+            table.getColumnModel().getColumn(1).setResizable(false);
+            table.getColumnModel().getColumn(2).setResizable(false);
+            table.getColumnModel().getColumn(3).setResizable(false);
+            table.getColumnModel().getColumn(4).setResizable(false);
+            table.getColumnModel().getColumn(5).setResizable(false);
+        }
+
+        btnDetail.setBackground(new java.awt.Color(255, 204, 0));
+        btnDetail.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        btnDetail.setText("Detail");
+        btnDetail.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDetailActionPerformed(evt);
+            }
+        });
+
+        btnCancel.setBackground(new java.awt.Color(255, 204, 0));
+        btnCancel.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        btnCancel.setText("Back");
+        btnCancel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelActionPerformed(evt);
+            }
+        });
+
+        lblDate.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        lblDate.setText("Incude Date");
+
+        cbtnDate.setBackground(new java.awt.Color(255, 255, 255));
+        cbtnDate.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        cbtnDate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbtnDateActionPerformed(evt);
+            }
+        });
+
+        calendarDate.setEnabled(false);
+
+        javax.swing.GroupLayout emptyPanelLayout = new javax.swing.GroupLayout(emptyPanel);
+        emptyPanel.setLayout(emptyPanelLayout);
+        emptyPanelLayout.setHorizontalGroup(
+            emptyPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(emptyPanelLayout.createSequentialGroup()
+                .addGroup(emptyPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(emptyPanelLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(scrollPaneForTable, javax.swing.GroupLayout.DEFAULT_SIZE, 786, Short.MAX_VALUE))
+                    .addGroup(emptyPanelLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btnDetail)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnCancel, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(emptyPanelLayout.createSequentialGroup()
+                .addGap(102, 102, 102)
+                .addComponent(lblStatus)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(cbbStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(cbtnDate, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblDate)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(calendarDate, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnSearch)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        emptyPanelLayout.setVerticalGroup(
+            emptyPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(emptyPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(emptyPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(emptyPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(lblStatus)
+                        .addComponent(cbbStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(emptyPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(lblDate)
+                        .addComponent(calendarDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnSearch))
+                    .addComponent(cbtnDate, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(scrollPaneForTable, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(emptyPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnDetail)
+                    .addComponent(btnCancel))
+                .addGap(99, 99, 99))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(emptyPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(109, Short.MAX_VALUE)
+                .addComponent(emptyPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 422, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(69, 69, 69))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSearchActionPerformed
+        int statusType = cbbStatus.getSelectedIndex();
+        Date date = calendarDate.getDate();
+        boolean isDateSelected = cbtnDate.isSelected();
+        controller.btnSearchOnShowReservedAndCheckedIn(this, statusType, date, isDateSelected);
+    }//GEN-LAST:event_btnSearchActionPerformed
+
+    private void btnDetailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDetailActionPerformed
+
+    }//GEN-LAST:event_btnDetailActionPerformed
+
+    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
+        controller.goToHome();
+    }//GEN-LAST:event_btnCancelActionPerformed
+
+    private void cbtnDateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbtnDateActionPerformed
+        if (cbtnDate.isSelected()) {
+            calendarDate.setEnabled(true);
+        } else {
+            calendarDate.setEnabled(false);
+        }
+    }//GEN-LAST:event_cbtnDateActionPerformed
+
+    public JTable getTable() {
+        return table;
+    }
+
+    public void setTable(JTable table) {
+        this.table = table;
+    }
+
+    public ArrayList<RestForm> getRests() {
+        return rests;
+    }
+
+    public void setRests(ArrayList<RestForm> rests) {
+        this.rests = rests;
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancel;
+    private javax.swing.JButton btnDetail;
+    private javax.swing.JButton btnSearch;
+    private org.freixas.jcalendar.JCalendarCombo calendarDate;
+    private javax.swing.JComboBox cbbStatus;
+    private javax.swing.JCheckBox cbtnDate;
+    private javax.swing.JPanel emptyPanel;
+    private javax.swing.JLabel lblDate;
+    private javax.swing.JLabel lblStatus;
+    private javax.swing.JScrollPane scrollPaneForTable;
+    private javax.swing.JTable table;
+    // End of variables declaration//GEN-END:variables
+}
